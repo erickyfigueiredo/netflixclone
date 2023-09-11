28fe7abf64946adabf3a541e053e1c57a2992195
ericky <erickyfigueiredo@gmail.com>
2023-09-11 11:53:46 +0900

    feat: api request

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Tmdb from "./Tmdb";
 
 export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const loadAll = async () => {
+
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+    }
+
+    loadAll();
+  },[]);
+
   return (
-    
+    <div className="page">
+      
+    </div>
   );
+
 }
