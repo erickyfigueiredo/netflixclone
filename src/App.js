4dbb1a4750a381bceafbaa25fec0c23b65ac8dac
ericky <erickyfigueiredo@gmail.com>
2023-09-17 18:52:55 +0900

    feat: create header component and fix: featuremovie component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
+import './App.css';
 import Tmdb from "./Tmdb";
 import MovieRow from "./components/MovieRow";
 import FeatureMovie from "./components/FeatureMovie";
+import Header from './components/Header';
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
   const [featureData, setFeatureData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
 
   useEffect(() => {
     const loadAll = async () => {
@@ -17,6 +20,9 @@ export default () => {
       let originals = list.filter(i=>i.slug === 'originals');
       let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
       let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeatureData(chosenInfo);
+      console.log(chosenInfo);
     }
 
     loadAll();
@@ -24,6 +30,8 @@ export default () => {
 
   return (
     <div className="page">
+
+      <Header></Header>
 
       {featureData && <FeatureMovie item={featureData}></FeatureMovie>}
 
--- src/Tmdb.js
+++ src/Tmdb.js
@@ -15,43 +15,62 @@ export default {
             {
                 slug: 'originals',
                 title: 'Netflix Originals',
-                items: await basicFetch(`/discover/tv?with_network=213&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/tv?with_network=213&language=en-US&api_key=${API_KEY}`)
             },
             {
                 slug: 'trending',
                 title: 'For you',
-                items: await basicFetch(`/trending/all/week?api_key=${API_KEY}`)
+                items: await basicFetch(`/trending/all/week?&language=en-US&api_key=${API_KEY}`)
             },
             {
                 slug: 'toprated',
                 title: 'Top Global',
-                items: await basicFetch(`/movie/top_rated?api_key=${API_KEY}`)
+                items: await basicFetch(`/movie/top_rated?&language=en-US&api_key=${API_KEY}`)
             },
             {
                 slug: 'action',
                 title: 'Action',
-                items: await basicFetch(`/discover/movie?with_genres=28&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/movie?with_genres=28&language=en-US&api_key=${API_KEY}`)
             },
             {
                 slug: 'comedy',
                 title: 'Comedy',
-                items: await basicFetch(`/discover/movie?with_genres=35&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/movie?with_genres=35&language=en-US&api_key=${API_KEY}`)
             },
             {
                 slug: 'horror',
                 title: 'Horror',
-                items: await basicFetch(`/discover/movie?with_genres=27&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/movie?with_genres=27&language=en-US&api_key=${API_KEY}`)
             },
             {
                 slug: 'romance',
                 title: 'Romance',
-                items: await basicFetch(`/discover/movie?with_genres=10749&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/movie?with_genres=10749&language=en-US&api_key=${API_KEY}`)
             },
             {
                 slug: 'documentary',
                 title: 'Documentary',
-                items: await basicFetch(`/discover/movie?with_genres=99&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/movie?with_genres=99&language=en-US&api_key=${API_KEY}`)
             },
         ]
+    },
+    getMovieInfo: async (movieId, type) => {
+        let info = {};
+
+        if(movieId) {
+            switch(type) {
+                case 'movie':
+                    info = await basicFetch(`/movie/${movieId}?language=en-US&api_key=${API_KEY}`);
+                break;
+                case 'tv':
+                    info = await basicFetch(`/tv/${movieId}?language=en-US&api_key=${API_KEY}`);
+                break;
+                default:
+                    info = null;
+                break;
+            }
+        }
+
+        return info;
     }
 }
