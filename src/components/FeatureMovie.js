867fba4336c6ccd505d79e55bb6baae06bebda12
ericky <erickyfigueiredo@gmail.com>
2023-09-17 20:04:55 +0900

    style: description max length

--- src/components/FeatureMovie.js
+++ src/components/FeatureMovie.js
@@ -9,6 +9,11 @@ export default ({item}) => {
         genres.push(item.genres[i].name);
     }
 
+    let description = item.overview;
+    if(description.length > 200) {
+        description = description.substring(0, 200) + '...';
+    }
+
     return (
         <section className="featured" style={{
             backgroundSize: 'cover',
@@ -24,7 +29,7 @@ export default ({item}) => {
                         <div className="featured--points">{item.vote_average} score</div>
                         <div className="featured--year">{firstDate.getFullYear()}</div>
                         <div className="featured--seasons">{item.number_of_seasons} season{item.number_of_seasons !== 1 ? 's' : ''}</div>
-                        <div className="featured--description">{item.overview}</div>
+                        <div className="featured--description">{description}</div>
                         <div className="featured--buttons">
                             <a href={`/watch/${item.id}`} className="featured--watchbutton">▶ Watch</a>
                             <a href={`/list/add/${item.id}`}className="featured--mylistbutton">+ My List</a>
